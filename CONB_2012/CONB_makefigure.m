a8133d6bdc97e4fd400f9634b1d3bc6d519f788f
jyeatman <jyeatman@stanford.edu>
2013-01-06 13:52:55 -0800

    updated documentation and code

--- CONB_2012/CONB_makefigure.m
+++ CONB_2012/CONB_makefigure.m
@@ -1,41 +1,59 @@
+%% This script will generate figure 1 from Wandell and Yeatman (2013)
+% The script requires the Automated Fiber Quantification (AFQ), VISTASOFT
+% and SPM software packages. More information can be found at 
+% http://white.stanford.edu/newlm/index.php/AFQ
+%
+% Copyright Jason D. Yeatman and Brian A. Wandell December 2012
+
 %% Load files
+
 % Get the subjects data directory
 sdir = fullfile(fileparts(which('CONB_makefigure')),'data','s1');
-% Get the callosum arcuate and ilf fiber groups
+% Load the callosum, arcuate and ILF fiber groups
 cc = dtiLoadFiberGroup(fullfile(sdir,'Reading_Fibers','AngularGyrusCallosum.pdb'));
 af = dtiLoadFiberGroup(fullfile(sdir,'Reading_Fibers','L_Arcuate_clean.pdb'));
 ilf = dtiLoadFiberGroup(fullfile(sdir,'Reading_Fibers','L_ILF_cleaned.pdb'));
-
-%% Render Fibers
-% Render the fibers
-AFQ_RenderFibers(af,'color',[0 .5 1],'numfibers',100)
-AFQ_RenderFibers(ilf,'color',[1 .5 0],'numfibers',100,'newfig',0)
-AFQ_RenderFibers(cc,'color',[0 .8 .2],'numfibers',100,'newfig',0)
-% Add an slices from the t1 volume to the plots
+% Load the subject's t1 weighted image
 t1 = readFileNifti(fullfile(sdir,'t1','t1.nii.gz'));
-% Slice x = -1
+
+%% Render the Fibers
+
+% Render the arcuate fasciculus in a new figure window. For the sake of
+% computation time 100 fibers will be chosen at random for rendering. The
+% output h is a handle for the light object in the figure window.
+h = AFQ_RenderFibers(af,'color',[0 .5 1],'numfibers',100);
+% Render the ILF in the same figure window
+AFQ_RenderFibers(ilf,'color',[1 .5 0],'numfibers',100,'newfig',0);
+% Render the corpus callosum in the same figure window
+AFQ_RenderFibers(cc,'color',[0 .8 .2],'numfibers',100,'newfig',0);
+
+%% Add a t1 weighted anatomy image as the background
+
+% Add slice x = -1
 AFQ_AddImageTo3dPlot(t1,[-1 0 0]);
 % And slice z = -15
 AFQ_AddImageTo3dPlot(t1,[0 0 -15]);
 
-%% Render another image with fMRI activation from a rhyming task
-% Add functional overlay of rhyming contrast
-AFQ_RenderFibers(af,'color',[0 .5 1],'numfibers',100)
-AFQ_RenderFibers(ilf,'color',[1 .5 0],'numfibers',100,'newfig',0)
-AFQ_RenderFibers(cc,'color',[0 .8 .2],'numfibers',100,'newfig',0)
-rhyme = readFileNifti(fullfile(sdir,'functional','Rhyme.nii.gz'));
-AFQ_AddImageTo3dPlot(t1,[0 1 0],[],[],[],'overlay',rhyme,[3 6]);
+%% Rotate the image around
 
-%% Render ROIs
-% % Load VWFA roi
-% vwfa = dtiReadRoi(fullfile(sdir,'Reading_ROIs','VWFA.mat'));
-% 
-% % Load phonology ROIs
-% temp = dtiReadRoi(fullfile(sdir,'Reading_ROIs','Phonology_Temporal.mat'));
-% front = dtiReadRoi(fullfile(sdir,'Reading_ROIs','Phonology_Frontal.mat'));
-% 
-% % Render these ROIs
-% AFQ_RenderRoi(vwfa,[1 0 0]);
-% AFQ_RenderRoi(temp,[1 0 0]);
-% AFQ_RenderRoi(front,[1 0 0]);
-
+% Tilt down 15 degrees
+camorbit(0,15);
+% Add a new light to the scene
+h = camlight(h,'right');
+% Rotate 90 degrees in incriments of 10
+for ii = 10:10:90
+    % Rotate the camera 10 degrees
+    camorbit(10,0);
+    % Rotate the light with the camera
+    camlight(h,'right');
+    % Pause for .5 seconds
+    pause(.5);
+end
+% Then rotate back to the starting position
+for ii = 10:10:90
+    camorbit(-10,0);
+    camlight(h,'right');
+    pause(.5);
+end
+% Tilt back up 15 degrees
+camorbit(0,-15)
